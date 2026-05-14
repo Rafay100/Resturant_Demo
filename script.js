97d7164c6d370c2cf3b68d2f13c2cf461bbc34fb
Rafay100 <srafay2021@gmail.com>
2026-05-14 13:27:58 +0500

    website updated

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-console.log("Website Loaded Successfully");
+console.log("Royal Spice Website Loaded");
 
-document.querySelectorAll("a").forEach(link => {
+const menuCards = document.querySelectorAll(".menu-card");
 
-  link.addEventListener("click", () => {
-    console.log("Navigation clicked");
+menuCards.forEach((card) => {
+
+  card.addEventListener("mouseenter", () => {
+
+    card.style.boxShadow =
+    "0 20px 50px rgba(0,0,0,0.5)";
+
+  });
+
+  card.addEventListener("mouseleave", () => {
+
+    card.style.boxShadow = "none";
+
   });
 
 });
+
+window.addEventListener("scroll", () => {
+
+  const nav = document.querySelector("nav");
+
+  if(window.scrollY > 50){
+
+    nav.style.background = "#000";
+
+  }
+  else{
+
+    nav.style.background =
+    "rgba(0,0,0,0.5)";
+
+  }
+
+}); 
